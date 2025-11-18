e8571d6f16c20e492ed8bc0bcab24c49ff4bf167
Pranav Deevi <u7923966@anu.edu.au>
2025-11-18 19:04:13 +1100

    Created basic universal hashing and init() as foundation for our future perfect hashing methods

--- src/hash.c
+++ src/hash.c
@@ -5,6 +5,8 @@
 #include "ph.h"
 #include "hash.h"
 
+#define PRIME 2147483647u
+
 /* Build first level buckets */
 static void build_buckets(ph_table *t, char **keys, size_t n) { 
     // one bucket per key (on avg)
@@ -99,3 +101,60 @@ void ph_free(ph_table *t) {
     free(t); 
 }
 
+
+
+
+
+/** 
+ * 
+ * @brief A randomised algo for constructing hash functions 
+ *        such that the prob of collision is ~ 1/M (theoretically). 
+ * 
+ * @param key - The String to hash 
+ * @param rand_coeff - The array of random coefficients (a[i])
+ * @param rand_additive_const - The random additive constant (b)
+ * @param prime - The chosen (large) prime value to perform mod with 
+ * @param table_size - Size of our hash table
+ * 
+ * @return integer hash value between [0, table_size] 
+ * 
+ */
+unsigned int universal_hash(char* key, Universal_Hash_Params* params) { 
+    
+    unsigned long long hash = params->rand_additive; 
+
+    for(int i = 0;  key[i] != '\0'; i++) { 
+        unsigned int temp = (unsigned char) key[i]; 
+        hash += (unsigned long long)temp * params->coeff_array[i]; 
+        // optional - reduce mod p to avoid overflow
+    }
+    return (unsigned int)((hash % params->prime) % params->table_size);
+}
+
+/** 
+ * 
+ * @brief Initialises the params for our universal hash function and 
+ *        sets key size limits and table size limits. Note that it 
+ *        also creates the random coefficient array that is used to 
+ *        ensure low prob of collisions (primarily <= 1/table_size). 
+ * 
+ * @param params Location to store all params 
+ * @param table_size size of to be hash table 
+ * @param max_str_len max length of future input keys 
+ * 
+ */
+void init_universal_hash(Universal_Hash_Params* params, unsigned int table_size, unsigned int max_str_len) { 
+    
+    params->prime = PRIME; 
+    params->table_size = table_size; 
+    params->max_str_len = max_str_len; 
+
+    params->coeff_array = malloc(sizeof(unsigned int) * max_str_len); 
+
+    params->rand_additive = (unsigned int)(rand() % params->prime); 
+
+    for(int i = 0; i < max_str_len; i++) { 
+        unsigned int r = (unsigned int) rand(); 
+        params->coeff_array[i] = (r % (params->prime - 1)) + 1; 
+    }
+}
